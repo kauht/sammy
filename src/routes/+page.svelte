052297e24f2d38f2fd992016ffb5095a04a30c10
kauht <macinnes.samuel1@gmail.com>
2026-02-15 14:32:16 -0600

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav.svelte';
+	import Background from '$lib/components/Background.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Background>
+	<Nav />
+	<main class="relative z-10 mx-auto max-w-3xl p-8 md:p-16"></main>
+	<Footer />
+</Background>
